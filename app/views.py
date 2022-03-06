7e4a632299704bb8b5b4a6eca6a414f8f3422f41
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-06 15:43:39 +0300

    Route: added route for each pitch

--- app/views.py
+++ app/views.py
@@ -1,31 +1,40 @@
 from crypt import methods
 from unicodedata import category
-from flask import Blueprint,render_template,abort,request
+from flask import Blueprint, render_template, abort, request
 from flask_login import login_required, current_user
 from app.models import Pitches, User
 from . import db
 
 
-views = Blueprint('views',__name__)
+views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def index():
 
     return render_template('index.html')
 
-@views.route('/pitches',methods = ['GET','POST'])
+
+@views.route('/pitches', methods=['GET', 'POST'])
 @login_required
 def pitches():
     users = User.query.all()
-
+    pitches = Pitches.get_pitches()
     if request.method == 'POST':
         title = request.form.get('title')
         content = request.form.get('pitch')
         category = request.form.get('catselect')
 
-        new_pitch = Pitches(title = title,content = content,user_id = current_user.id,categories_id = category)
+        new_pitch = Pitches(title=title, content=content,
+                            user_id=current_user.id, categories_id=category)
         new_pitch.save_pitch()
-    return render_template('pitches.html',users=users, user = current_user)
-        
+    return render_template('pitches.html', users=users, user=current_user, pitches=pitches)
 
-    
+
+@views.route('/pitch/<int:id>')
+def single_pitch(id):
+    pitch = Pitches.query.get(id)
+
+    return render_template('pitch.html',pitch=pitch)
+
+
