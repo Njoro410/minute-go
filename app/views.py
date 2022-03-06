08e8a60864b6b8bdcc3f04bdfa80e68ccaf66c79
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-06 23:23:45 +0300

    Routes:added routes and methods save comment,save profile,update profile

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from crypt import methods
 from unicodedata import category
-from flask import Blueprint, render_template, abort, request
+from flask import Blueprint, render_template, abort, request, redirect, url_for
 from flask_login import login_required, current_user
-from app.models import Pitches, User
-from . import db
+from app.forms import UpdateProfile
+from app.models import Pitches, User, Comments
+from . import db,photos
 
 
 views = Blueprint('views', __name__)
@@ -35,6 +36,56 @@ def pitches():
 def single_pitch(id):
     pitch = Pitches.query.get(id)
 
-    return render_template('pitch.html',pitch=pitch)
+    return render_template('pitch.html', pitch=pitch, user=current_user)
 
 
+@views.route('/pitch', methods=['GET', 'POST'])
+def comments():
+    comments = Comments.get_comment()
+    if request.method == 'POST':
+        comment = request.form.get('comment')
+
+        new_comment = Comments(
+            comment=comment, user_id=current_user.id, pitch_id=id)
+        new_comment.save_comment()
+    return render_template('pitch.html', comment=comments)
+
+
+@views.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
+
+@views.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@views.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('views.profile',uname=uname))
