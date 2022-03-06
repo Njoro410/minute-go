35ddb497df55f2d68e1539eafef03387fb9735cc
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-06 13:52:15 +0300

    added method to save pitches

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint,render_template
-
+from crypt import methods
+from unicodedata import category
+from flask import Blueprint,render_template,abort,request
+from flask_login import login_required, current_user
+from app.models import Pitches, User
+from . import db
 
 
 views = Blueprint('views',__name__)
@@ -8,3 +12,20 @@ views = Blueprint('views',__name__)
 def index():
 
     return render_template('index.html')
+
+@views.route('/pitches',methods = ['GET','POST'])
+@login_required
+def pitches():
+    users = User.query.all()
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('pitch')
+        category = request.form.get('catselect')
+
+        new_pitch = Pitches(title = title,content = content,user_id = current_user.id,categories_id = category)
+        new_pitch.save_pitch()
+    return render_template('pitches.html',users=users, user = current_user)
+        
+
+    
