6632b9243220cd7ec2f8751dd3b174a2755a209d
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-09 08:08:51 +0300

    Comment: added comment form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Required
 
+
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    content = TextAreaField('Comment',validators=[Required()])
     submit = SubmitField('Submit')
